40e3f28870057f709c976be7b1925100155b4135
Christian Baumann <chriss713@gmail.com>
2021-04-16 14:44:24 +0200

    Added tests for multiplication

--- tests/test_math.py
+++ tests/test_math.py
@@ -14,3 +14,12 @@ def test_divide_by_zero():
         num = 1 / 0
 
     assert 'division by zero' in str(e.value)
+
+def test_multiply_two_positive_ints():
+    assert 2 * 3 == 6
+
+def test_multiply_identity():
+    assert 1 * 99 == 99
+
+def test_multiply_zero():
+    assert 0 * 100 == 0
